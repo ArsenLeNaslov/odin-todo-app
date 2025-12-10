eb05e13dbc679d73b1d946ecf6f50b2bc907f323
Arsén <rhos.andrea@gmail.com>
2025-12-10 16:48:36 +0100

    - Set entry point to src/index.js and output   bundle to dist/bundle.js with automatic cleaning - Add HtmlWebpackPlugin to generate HTML   from index.html template - Configure CSS support using style-loader   and css-loader - Enable webpack-dev-server to serve dist and   automatically open browser on start - Favicon reference

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      favicon: './src/daily-task.png'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+    open: true,
+  },
+};
